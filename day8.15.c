e65d5151c051185ad70ef2b264c49da7e836beea
dorime <aswalrishika09@gmail.com>
2025-11-20 20:11:15 +0530

    Add character classification program in C

---
+++ day8.15.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int main(){
+	char ch;
+	printf("enter the character");
+	scanf("%c",&ch);
+	if(ch>='a' && ch<='z'){
+		printf("lowercase alphabet");
+	}
+	else if(ch>='A' && ch<='Z'){
+		printf("uppercase alphabet");
+	}
+	else if(ch>=0 && ch<=9){
+		printf("digit");
+	}
+	else{
+		printf("special character");
+	}
+}
