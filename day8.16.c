39330baeaca92de4b3285f27629866f64e1853d9
dorime <aswalrishika09@gmail.com>
2025-11-20 20:12:18 +0530

    Add program to find the largest of three numbers

---
+++ day8.16.c
@@ -0,0 +1,20 @@
+
+#include<stdio.h>
+int main(){
+	int a,b,c;
+	printf("enter the first number");
+	scanf("%d",&a);
+	printf("enter the second number");
+	scanf("%d",&b);
+	printf("enter the third number");
+	scanf("%d",&c);
+	if(a>b&&a>c){
+		printf(" first number is largest");
+	}
+	else if(b>a&&b>c){
+		printf("second number is largest");
+	}
+	else{
+		printf(" third number is largest");
+	}
+}
